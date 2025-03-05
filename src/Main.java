0646fa97c7fb5054c9d2300c5a122822361835e1
Maribu <maribu2411@gmail.com>
2025-03-05 04:00:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int initialBalance = 250;
+        int added = 1500;
+
+        int bonus = 0;
+        if (added > 1000) {
+            bonus = added / 100;
+        }
+        int finalBalance = initialBalance + added + bonus;
+        System.out.println("Итоговый счет: " + finalBalance);
+        System.out.println("Бонус: " + bonus);
+    }
+}
+gi
